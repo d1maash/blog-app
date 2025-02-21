f7722a65fac07fb2d647dee585f66be042c81256
Dimash <dimashaijarikov@gmail.com>
2025-02-21 20:03:25 +0500

    update

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -17,9 +17,26 @@ const Navbar = () => {
                     {open ? "X" : "="}
 
                 </div>
+                <div className={`w-full h-screen flex flex-col items-center justify-center gap-8 font-medium text-lg absolute top-16 bg-[#e6e6ff] transition-all ease-in-out ${open ? "-right-0" : "-right-[100%]"}`}>
+                    <a href="/">Home</a>
+                    <a href="/">Trending</a>
+                    <a href="/">Most Populer</a>
+                    <a href="/">About</a>
+                    <a href="">
+                        <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">Login 👋</button>
+                    </a>
+                </div>
             </div>
             {/* deskt-m */}
-            <div className="hidden md:flex">D</div>
+            <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
+                <a href="/">Home</a>
+                <a href="/">Trending</a>
+                <a href="/">Most Populer</a>
+                <a href="/">About</a>
+                <a href="">
+                    <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">Login 👋</button>
+                </a>
+            </div>
         </div>
 
     )
